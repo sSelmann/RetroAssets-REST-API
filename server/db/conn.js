0ec80680f9e3a8c0874525896984b45751c20b7a
Sellman <56926590+sSelmann@users.noreply.github.com>
2022-12-04 15:02:52 +0300

    Connection to database established and first route added

--- server/db/conn.js
+++ server/db/conn.js
@@ -14,7 +14,7 @@ module.exports = {
         return callback(err);
       }
 
-      dbConnection = db.db('sample_airbnb');
+      dbConnection = db.db('test');
       console.log('Successfully connected to MongoDB.');
 
       return callback();
