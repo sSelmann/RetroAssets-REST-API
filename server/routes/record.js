b23e0adce7a182ea50c2c4d3207fdf904c1966d9
Sellman <56926590+sSelmann@users.noreply.github.com>
2022-12-10 23:51:28 +0300

    Added new record route

--- server/routes/record.js
+++ server/routes/record.js
@@ -9,11 +9,26 @@ const recordRoutes = express.Router();
 const dbo = require('../db/conn');
 
 // This section will help you get a list of all the records.
+recordRoutes.route('/tablist').get(async function (_req, res) {
+  const dbConnect = dbo.getDb();
+
+  dbConnect
+    .collection('tabs')
+    .find({})
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send('Error fetching listings!');
+      } else {
+        res.json(result);
+      }
+    });
+});
+
 recordRoutes.route('/pagelist').get(async function (_req, res) {
   const dbConnect = dbo.getDb();
 
   dbConnect
-    .collection('users')
+    .collection('pages')
     .find({})
     .toArray(function (err, result) {
       if (err) {
